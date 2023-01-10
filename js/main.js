87b6874d98068f5a9d4863fcc7ba3ab5998ecd54
aykat1995 <54204536+aykat1995@users.noreply.github.com>
2023-01-10 23:54:36 +1100

    Second commit

--- js/main.js
+++ js/main.js
@@ -6,10 +6,27 @@ let headerMenu = document.querySelector('.header-menu');
 let menuBurger = document.querySelector('.menu-burger');
 const pageWidth = document.documentElement.scrollWidth;
 
-menuBurger.addEventListener('click', function(event){
-  event.preventDefault();
+menuBurger.addEventListener('click', function(e){
+  e.preventDefault();
   headerMenu.classList.toggle('open');
-  
+})
+
+
+// Переключение языков
+
+let langBox = document.querySelector('.header-top-lang')
+let langItems = document.querySelectorAll('.header-top-lang__item')
+console.log(langItems)
+
+langBox.addEventListener('click', function(e) {
+  e.preventDefault();
+  let selected = e.target
+
+  if (!selected.classList.contains('header-top-lang__item')) return
+
+  let activeLang = selected
+  langItems.forEach(item => item.classList.remove('active'))
+  activeLang.classList.add('active')
 })
 
 });
